4243f87f3f2711f222bf116479f5501548431961
Minh Nguyens <minhnguyen@wee.vn>
2022-02-28 23:07:58 +0700

    update config db

--- index.php
+++ index.php
@@ -4,6 +4,21 @@
 <title>How to put PHP in HTML - Simple Example</title>
 </head>
 <body>
-<h1><?php echo "This message is from server side." ?></h1>
+<?php
+$servername = "us-cdbr-east-05.cleardb.net";
+$username = "bbd2db5e7c59ce";
+$password = "0e63ed34";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+echo "Hello from DB";
+
+?>
 </body>
 </html>
