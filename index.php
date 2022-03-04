95f2a1afae9141ac4104fc7ac78612105f7ef1c8
Minh Nguyens <minhnguyen@wee.vn>
2022-03-05 00:58:39 +0700

    update upload, delete to Cloudinary using php

--- index.php
+++ index.php
@@ -8,20 +8,38 @@
   <body>
     <div class="card">
       <img src="http://fillmurray.com/g/300/300" id="img-preview" />
+      <form method="post" action="test.php" enctype='multipart/form-data' >
       <label class="file-upload-container" for="file-upload">
-        <input id="file-upload" type="file" style="display:none;">
+        <input id="file-upload" type="file" style="display:none;" name="file">
         Select your music
       </label>
+      <input  type="text" name = 'text'>
+      <input id="submitBtn"  type="submit" onsubmit='return onFormSubmit(this);'>
+</form>
+
+<script>
+    function onFormSubmit(form) {
+        console.log(form);
+        // // save in cookies
+        // document.cookie = "my_form=" + jsonData;
+ 
+        // // redirect to form's action
+        // window.location.href = form.getAttribute("action");
+ 
+        // prevent form from submitting
+        return false;
+    }
+</script>
+
+
+
       <audio id= "audio-render" controls autoplay>
-  <source
-    
-    type="audio/mpeg"
-  />
+  <source type="audio/mpeg" />
   Your browser does not support the audio element.
 </audio>
     </div>
     <!-- Scripts -->
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-    <script src="./app.js"></script>
+    <!-- <script src="./app.js"></script> -->
   </body>
 </html>
