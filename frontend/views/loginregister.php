659c1e2bfa58abfb028c2ba2be2bc77d3b5b1920
Minh Nguyens <minhnguyen@wee.vn>
2022-03-05 13:29:27 +0700

    structure project coding convetion

---
+++ frontend/views/loginregister.php
@@ -0,0 +1,20 @@
+<?php
+    require_once __DIR__ . '/template/header.php';
+    // Display register
+    require_once __DIR__ . '/register/register.php';
+
+    // Display login
+    require_once __DIR__ . '/login/login.php';
+
+    // Wait for 5s before redirect to Home
+    echo "<script>
+        setTimeout(()=>{
+            document.location.href='".$__DIR__."/frontend/views/home.php';
+        },5000)
+    </script>";
+
+    // // Redirect to home
+    // echo "<script type='text/javascript'>document.location.href='".$__DIR__."/frontend/views/home.php';</script>";
+    require_once __DIR__ . '/template/footer.php';
+?>
+
---
+++ frontend/views/musicplayer/musicplayer.php
@@ -0,0 +1,2 @@
+<!-- musicplayer display and handle to backend -->
+<h1>Hi, i'm MusicPlayer</h1>
