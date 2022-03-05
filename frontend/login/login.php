f9fc6dd54f50cabbead17b5647a2f5ad99ebcac4
Khuong <s3924577@rmit.edu.vn>
2022-03-05 21:21:15 +0700

    update interface of login and register page

--- frontend/login/login.php
+++ frontend/login/login.php
@@ -1,2 +1,28 @@
-<!-- login display and handle to backend -->
-<h1>Hi, i'm Login</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container">
+        <form class="login">
+            <p class="lbl">Login</p>
+            <div class="input_group">
+                <input type="username" placeholder="Username" required>
+            </div>
+            <div class="input_group">
+                <input type="password" placeholder="Password" required>
+            </div>
+            <button class="btn">Login</button>
+            <p class="direct_text">Don't have an account? <a class="direct_btn" href="register.php">Register Now.</a></p>
+        </form>
+    </div>
+</body>
+</html>
--- frontend/register/register.php
+++ frontend/register/register.php
@@ -1,2 +1,34 @@
-<!-- register display and handle to backend -->
-    <h1>Hi, i'm Register</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="register.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+    <title>Register</title>
+</head>
+<body>
+    <div class="container">
+        <form class="register">
+            <p class="lbl">Register</p>
+            <div class="input_group">
+                <input type="name" placeholder="Name" required>
+            </div>
+            <div class="input_group">
+                <input type="username" placeholder="Username" required>
+            </div>
+            <div class="input_group">
+                <input type="password" placeholder="New password" required>
+            </div>
+            <div class="input_group">
+                <input type="con_password" placeholder="Confirm password" required>
+            </div>
+            <button class="btn">Register</button>
+            <p class="direct_text">Already have an account? <a class="direct_btn" href="login.php">Login here!</a></p>
+        </form>
+    </div>
+</body>
+</html>
